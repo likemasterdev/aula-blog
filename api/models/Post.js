adb05afe91799928316cf9ab70c17cd90bf12523
Like Master Development <likemasterdev@gmail.com>
2019-09-11 14:49:04 -0300

    Aula 01

---
+++ api/models/Post.js
@@ -0,0 +1,18 @@
+/**
+ * Post.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    titulo: { type: 'string', required: true },
+    descricao: { type: 'string', required: true }
+
+  },
+
+};
+
